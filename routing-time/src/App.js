66c0d27a1ce943beaee3253c1137144536fe42c1
brockpettyjohn <brockpettyjohn@gmail.com>
2017-09-20 13:55:48 -0600

    done

--- routing-time/src/App.js
+++ routing-time/src/App.js
@@ -5,30 +5,36 @@ import Home from './Home.js'
 import About from './About.js'
 import Secret from './Secret.js'
 import Odor from './Odor.js'
-import BadHair from './BadHair.js'
 import SecretsLanding from './SecretsLanding.js'
-
+import BadHair from './BadHair.js'
+import { Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HashRouter>
-          <div>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/secret'>Secret</Link>
-            <Route exact path='/' component={ Home }  />
-            <Route path='/about' component={ About } />
-            <Route path='/secret' render={()=>(
-              <Secret>
-                <Link to='/secret/odor'>Secret #1</Link>
-                <Link to='/secret/badhair'>Secret #2</Link>
-                <Route path='/secret/odor' component={Odor} />
-                <Route path='/secret/badhair' component={BadHair} />
-                <Route component={SecretsLanding} />
-              </Secret>)} />
-          </div>
+          <Switch>
+            <div className='links'>
+              <Link to='/'>Home</Link>
+              <Link to='/about'>About</Link>
+              <Link to='/secret'>Secret</Link>
+
+              <Route exact path='/' component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/secret' render={() => (
+                <Secret>
+                  <Link to='/secret/badhair'>Secret #2</Link>
+                  <Link to='/secret/odor'>Secret #1</Link>
+                  <Switch>
+                    <Route path='/secret/odor' component={Odor} />
+                    <Route path='/secret/badhair' component={BadHair} />
+                    <Route component={SecretsLanding} />
+                  </Switch>
+
+                </Secret>)} />
+            </div>
+          </Switch>
         </HashRouter>
       </div>
     );
