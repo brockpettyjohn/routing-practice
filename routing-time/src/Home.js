d647407eafef9306acbd627e031e8419c428563d
brockpettyjohn <brockpettyjohn@gmail.com>
2017-09-20 08:19:06 -0600

    done

--- routing-time/src/Home.js
+++ routing-time/src/Home.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Home extends Component{
-    render(){
+export default function Home (){
         return(
         <div>
             <p>This is the Home page!</p>
+            <img className='home'alt='houses' src='https://goldinauctions.com/ItemImages/000007/7544d_lg.jpeg' />
         </div>
         )
         
     }
-}
-export default Home
+
